3320126a0bcece786133df4e4792f90202ae5985
becored <becored0@gmail.com>
2023-05-19 18:01:36 +0900

    update layer importer/exporter

--- scripts/ExportLayerDataAllRooms.js
+++ scripts/ExportLayerDataAllRooms.js
@@ -1,14 +1,19 @@
-function ExportLayerDataAllRooms()
-{
-    var output_dir = "./#Layers/";
+function ExportLayerDataAllRooms() {
 
-    for (current_layer = 0; current_layer < 3; current_layer ++) {
-        for (current_room_id = 0; current_room_id < WL4EditorInterface.GetRoomNum(); current_room_id ++) {
+    var output_dir = "./#Layers/";
+    var rooms_info = [];
+
+    rooms_info.push(WL4EditorInterface.GetRoomNum());
+
+    for (current_room_id = 0; current_room_id < WL4EditorInterface.GetRoomNum(); current_room_id++) {
+        for (current_layer = 0; current_layer < 3; current_layer++) {
             WL4EditorInterface.SetCurrentRoomId(current_room_id);
             WL4EditorInterface._ExportLayerData(output_dir + "Room" + ("00" + WL4EditorInterface.GetCurRoomId()).slice(-2) + "_" + current_layer + ".bin", current_layer);
             WL4EditorInterface.log("File exported:" + output_dir + "Room" + ("00" + WL4EditorInterface.GetCurRoomId()).slice(-2) + "_" + current_layer + ".bin");
         }
+        rooms_info.push(WL4EditorInterface.GetCurRoomLayerWidth(1));
+        rooms_info.push(WL4EditorInterface.GetCurRoomLayerHeight(1));
     }
+    WL4EditorInterface.WriteTxtFile(output_dir + "Rooms_info.txt", rooms_info);
 }
-
 ExportLayerDataAllRooms();
--- scripts/ImportLayerDataAllRooms.js
+++ scripts/ImportLayerDataAllRooms.js
@@ -1,10 +1,22 @@
-function ImportLayerDataAllRooms()
-{
-    var output_dir = "./#Layers/";
+function ImportLayerDataAllRooms() {
 
-    for (current_layer = 0; current_layer < 3; current_layer ++) {
-        for (current_room_id = 0; current_room_id < WL4EditorInterface.GetRoomNum(); current_room_id ++) {
-            WL4EditorInterface.SetCurrentRoomId(current_room_id);
+    var output_dir = "./#Layers/";
+    var str;
+    var rooms_info = [];
+
+    str = WL4EditorInterface.ReadTxtFile(output_dir + "Rooms_info.txt");
+    rooms_info = str.split(',');
+
+    if (WL4EditorInterface.GetRoomNum() != rooms_info[0]) {
+        WL4EditorInterface.log("Number of rooms does not match!");
+        return;
+    }
+
+    for (current_room_id = 0; current_room_id < WL4EditorInterface.GetRoomNum(); current_room_id++) {
+        WL4EditorInterface.SetCurrentRoomId(current_room_id);
+        WL4EditorInterface.SetRoomSize(rooms_info[1+(current_room_id*2)],rooms_info[1+(current_room_id*2)+1],rooms_info[1+(current_room_id*2)],rooms_info[1+(current_room_id*2)+1]);
+        WL4EditorInterface.log("Room" + ("00" + WL4EditorInterface.GetCurRoomId()).slice(-2) + " size changed:" + rooms_info[1+(current_room_id*2)] + "," + rooms_info[1+(current_room_id*2)+1]);
+        for (current_layer = 0; current_layer < 3; current_layer++) {
             WL4EditorInterface._ImportLayerData(output_dir + "Room" + ("00" + WL4EditorInterface.GetCurRoomId()).slice(-2) + "_" + current_layer + ".bin", current_layer);
             WL4EditorInterface.log("File imported:" + output_dir + "Room" + ("00" + WL4EditorInterface.GetCurRoomId()).slice(-2) + "_" + current_layer + ".bin");
         }
